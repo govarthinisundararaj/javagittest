36c80bc7596da2d307b2ac0f99e1e95888081746
670334 <670334@PC223504.cts.com>
2018-01-03 18:17:29 +0530

    added new logic

--- Addition/src/Add.java
+++ Addition/src/Add.java
@@ -4,11 +4,14 @@ public class Add {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a,b;
+		int a,b,i;
+		int n=5;
 		//q
 		a = sc.nextInt();
 		b = sc.nextInt();
 		System.out.print("sum = "+(a+b));
+		for(i=0;i<n;i++)
+			System.out.println("qwerty");
 		//q
 	}
 
