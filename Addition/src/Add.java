7a1ee2ed8af1a5b90ffab54830beabbb23288d9d
670334 <670334@PC223504.cts.com>
2018-01-03 18:10:10 +0530

    nope change

---
+++ Addition/src/Add.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Add {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.print("sum = "+(a+b));
+	}
+
+}
