d2d5e1505544a131f5a9226e00e8c4312217aa3f
670334 <670334@PC223504.cts.com>
2018-01-03 18:11:51 +0530

    added comment in add.java

--- Addition/src/Add.java
+++ Addition/src/Add.java
@@ -5,6 +5,7 @@ public class Add {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a,b;
+		//q
 		a = sc.nextInt();
 		b = sc.nextInt();
 		System.out.print("sum = "+(a+b));
